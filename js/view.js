cde801ed4efdaa70c9e35cfa818e663a82c83839
kvonderhaar-FM <kvonderhaar@fabricatedmetals.com>
2025-07-17 16:19:34 -0400

    Delete js directory

--- js/view.js
+++
@@ -1,9 +0,0 @@
-class QuoteView {
-  constructor(outputId) {
-    this.outputEl = document.getElementById(outputId);
-  }
-
-  updateQuoteDisplay(price) {
-    this.outputEl.textContent = `Total: $${price}`;
-  }
-}
